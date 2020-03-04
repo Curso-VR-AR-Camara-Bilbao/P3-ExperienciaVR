e8dc944e6c92beab300b159a30924d96312b0e20
Angel Rafael Rubio <arfipod@gmail.com>
2020-03-04 22:42:51 +0100

    Agregada hoguera

---
+++ P3-ExperienciaVR/Assets/_P3-ExperienciaVR/Scripts/HogueraEncendible.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HogueraEncendible : MonoBehaviour
+{
+  public List<GameObject> ComponentesLlama;
+
+  public bool IsLlamaEncendida;
+
+  private void Start()
+  {
+    if (IsLlamaEncendida)
+    {
+      EncenderLlama();
+    }
+    else
+    {
+      ApagarLlama();
+    }
+  }
+
+  //Se podrían hacer ambos en un Script polivalente
+  public void EncenderLlama()
+  {
+    for(int i=0; i<ComponentesLlama.Count; i++)
+    {
+      ComponentesLlama[i].SetActive(true);
+    }
+    IsLlamaEncendida = true;
+  }
+
+  public void ApagarLlama()
+  {
+    for (int i = 0; i < ComponentesLlama.Count; i++)
+    {
+      ComponentesLlama[i].SetActive(false);
+    }
+    IsLlamaEncendida = false;
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    GameObject ObjetoAtravesado = other.gameObject;
+    Llama LlamaObjeto = ObjetoAtravesado.GetComponent<Llama>();
+    if (LlamaObjeto != null)
+    {
+      Invoke("EncenderLlama", 1);
+    }
+  }
+
+}
