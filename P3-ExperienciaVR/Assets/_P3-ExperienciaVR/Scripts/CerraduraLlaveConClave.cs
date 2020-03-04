dee8253ec7e3631ba12757eeafe0f97e4bc123da
Angel Rafael Rubio <arfipod@gmail.com>
2020-03-04 01:09:51 +0100

    P3 Avanzada hasta casi cierre de puerta

---
+++ P3-ExperienciaVR/Assets/_P3-ExperienciaVR/Scripts/CerraduraLlaveConClave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CerraduraLlaveConClave : MonoBehaviour
+{
+  public string Clave;
+
+  public UnityEvent OnClaveCorrecta;
+  public UnityEvent OnClaveIncorrecta;
+
+  private void OnTriggerEnter(Collision collision)
+  {
+    GameObject ObjectoColisionado = collision.gameObject;
+    if (ObjectoColisionado.GetComponent<LlaveConClave>())
+    {
+      if (ObjectoColisionado.GetComponent<LlaveConClave>().Clave == Clave) // Si la clave coincide
+      {
+        OnClaveCorrecta.Invoke();
+      }
+      else
+      {
+        OnClaveIncorrecta.Invoke();
+      }
+
+    }
+  }
+}
---
+++ P3-ExperienciaVR/Assets/_P3-ExperienciaVR/Scripts/PuertaAutomatica.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaAutomatica : MonoBehaviour
+{
+  public Animator animator;
+
+  private void Awake()
+  {
+    animator = GetComponent<Animator>();
+  }
+
+  public void Abrir()
+  {
+    animator.SetTrigger("Abriendose");
+  }
+
+  public void Cerrar()
+  {
+    animator.SetTrigger("Cerrandose");
+  }
+}
